10f9ec6bf670aadb1fb3a0d321994edc10e5f221
JAbozarOid <araghibdoust@hotmail.com>
2020-10-28 13:44:20 +0330

    services_repository

---
+++ lib/app/repository/data_repository.dart
@@ -0,0 +1,30 @@
+import 'package:cast/app/service/api_service.dart';
+import 'package:http/http.dart';
+
+class DataRepository {
+  final APIService apiService;
+
+  DataRepository(this.apiService);
+
+  Future<T> _getData<T>({Future<T> Function() onGetData}) async {
+    try {
+      return await onGetData();
+    } on Response catch (response) {
+      // invalid coordinates
+      if (response.statusCode == 400) {
+        return await onGetData();
+      }
+      // invalid api key
+      if (response.statusCode == 403) {
+        return await onGetData();
+      }
+      // not found
+      if (response.statusCode == 404) {
+        return await onGetData();
+      }
+      rethrow;
+    }
+  }
+
+
+}
---
+++ lib/app/service/api.dart
@@ -0,0 +1,53 @@
+import 'package:cast/app/service/api_key.dart';
+import 'package:flutter/foundation.dart';
+
+enum APIVersions { version }
+
+class API {
+  final String apiKey;
+
+  API({@required this.apiKey});
+
+  factory API.sandbox() => API(apiKey: APIKeys.castSandboxKey);
+
+  static final String host = '130.185.122.248:5000';
+
+  static Map<APIVersions, String> apiVersion = {
+    APIVersions.version: '/api/v1',
+  };
+}
+
+enum Path {
+  getSessionToken,
+  addFirebaseToken,
+  getMainCategoryList,
+  getCategoryListInDetail,
+  getVenueListByLocation,
+  getVenue,
+  getSavedVenueList,
+  getTravelTimeInfo
+}
+
+class PathApi {
+  static String getApiPath(Path path) {
+    switch (path) {
+      case Path.getSessionToken:
+        return "/Auth/GetSessionToken";
+      case Path.addFirebaseToken:
+        return "/Auth/AddFirebaseToken";
+      case Path.getMainCategoryList:
+        return "/Category/GetMainCategoryList";
+      case Path.getCategoryListInDetail:
+        return "/Category/GetCategoryListInDetail";
+      case Path.getVenueListByLocation:
+        return "/Venue/GetVenueListByLocation";
+      case Path.getVenue:
+        return "/Venue/GetVenue";
+      case Path.getSavedVenueList:
+        return "/Venue/GetSavedVenueList";
+      case Path.getTravelTimeInfo:
+        return "/TravelTimePredictor/GetTravelTimeInfo";
+    }
+    return "";
+  }
+}
